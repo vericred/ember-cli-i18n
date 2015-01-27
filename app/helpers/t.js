bf4d3bc6e7391a87834a99f2311ae20cac88547c
Dan Langevin <dan.langevin@gmail.com>
2015-01-27 13:38:21 -0500

    Fixing conflict from master

--- app/helpers/t.js
+++ app/helpers/t.js
@@ -5,20 +5,7 @@ export default function tHelper(params, hash, options, env) {
 
   var container = this.container;
   var t = container.lookup('utils:t');
-  var types = options.types;
-
-  // parse input params and streamify
-  for (var i = 0, l = args.length; i < l; i++) {
-    // (starting at 1 because we popped path off already
-    if (types[i + 1] === 'ID') {
-      args[i] = view.getStream(args[i]);
-    }
-  }
-
-  // convert path into a stream
-  if (types[0] === 'ID') {
-    path = view.getStream(path);
-  }
+  var application = container.lookup('application:main');
 
   var stream = new Stream(function() {
     return t(path, params);
@@ -29,7 +16,7 @@ export default function tHelper(params, hash, options, env) {
     var param = params[i];
     if(param && param.isStream){
       param.subscribe(stream.notify, stream);
-    }
+    };
   }
 
   container.localeStream.subscribe(stream.notify, stream);
